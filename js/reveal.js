7cb5334cb4f6921fbde35c31a0bbbf70313edcd8
Adrienne Tacke <adrienne.tacke@gmail.com>
2020-01-28 23:36:07 +0000

    fix: issue with code syntax highlighting; replaced with photos

--- js/reveal.js
+++ js/reveal.js
@@ -43,7 +43,7 @@
 			height: 1080,
 
 			// Factor of the display size that should remain empty around the content
-			margin: 0.01,
+			margin: 0.1,
 
 			// Bounds for smallest/largest possible scale to apply to content
 			minScale: 0.2,
